972567838e31106dc96185dac19dc753d10d5c3b
ArtcalO <niyca14@gmail.com>
2021-01-13 17:26:46 +0100

    Handling components issues

--- src/views/Slam.vue
+++ src/views/Slam.vue
@@ -4,18 +4,11 @@
       <div class="album py-5 bg-light">
           <div class="container">
             <div class="row">
-              <div v-for="posts in APIData" :key="posts.id" class="col-md-4">
+              <div v-for="slam in APIData" :key="slam.id" class="col-md-4">
                 <div class="card mb-4 box-shadow">
-                  <img class="card-img-top" src="https://via.placeholder.com/150x100" alt="Card image cap">
                   <div class="card-body">
-                      <h4 class=""><a class="text-secondary" href="">{{posts.title}}</a></h4>
-                      <p class="card-text">{{posts.content}}</p>
-                      <div class="d-flex justify-content-between align-items-center">
-                      <div class="btn-group">
-                      <a href="" class="btn btn-sm btn-outline-primary" role="button" aria-pressed="true">View</a>
-                      <a href="" class="btn btn-sm btn-outline-secondary" role="button" aria-pressed="true">Edit</a>
-                      </div>
-                      <small class="text-muted">9 mins</small>
+                      <h4 class=""><a class="text-secondary" href="">{{slam.title}}</a></h4>
+                      <p class="card-text">{{ slam.content }}</p>
                     </div>
                   </div>
                 </div>
@@ -28,22 +21,16 @@
 
 <script>
   import NavBar from '../components/Navbar'
-  import { getAPI } from '../axios-api'
+  import { baseAPI } from '../axios-api'
   import { mapState } from 'vuex'
   export default {
-    name: 'Posts',
-    onIdle () {
-      this.$store.dispatch('userLogout')
-        .then(() => {
-          this.$router.push({ name: 'login' })
-        })
-    },
+    name: 'Slam',
     components: {
       NavBar
     },
     computed: mapState(['APIData']),
     created () {
-        getAPI.get('/posts/', { headers: { Authorization: `Bearer ${this.$store.state.accessToken}` } })
+        baseAPI.get('/slam/')
           .then(response => {
             this.$store.state.APIData = response.data
           })
@@ -51,7 +38,7 @@
             console.log(err)
           })
     }
-  }
+  };
 </script>
 
 <style>
